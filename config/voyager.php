3f25004cde92522702e2591c5bdc5fff7b8dd759
Trịnh Hữu Vũ <vutrinhhuu@gmail.com>
2018-11-07 21:34:34 +0700

    Manage category and product

--- config/voyager.php
+++ config/voyager.php
@@ -161,8 +161,8 @@ return [
 
         'widgets' => [
             'TCG\\Voyager\\Widgets\\UserDimmer',
-            'TCG\\Voyager\\Widgets\\PostDimmer',
-            'TCG\\Voyager\\Widgets\\PageDimmer',
+            'App\\Widgets\\Products',
+            'App\\Widgets\\Categories',
         ],
 
     ],
