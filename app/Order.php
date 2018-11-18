ed08051da235850c1bdc877ea04306a667d2cb31
Trịnh Hữu Vũ <vutrinhhuu@gmail.com>
2018-11-18 09:53:11 +0700

    Complete admin_order_func

--- app/Order.php
+++ app/Order.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    //
+    public function details()
+    {
+        return $this->hasMany(OrderDetail::class);
+    }
+
+    public function deliver_address()
+    {
+        return $this->hasOne(DeliverAddress::class);
+    }
 }
