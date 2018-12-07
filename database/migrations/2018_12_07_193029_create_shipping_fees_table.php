98d802bec53c2aec74ea21f3c3fd84a01ec75fb5
Quoc Toan <quoctoan06@gmail.com>
2018-12-08 02:46:12 +0700

    update db for round 3

---
+++ database/migrations/2018_12_07_193029_create_shipping_fees_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateShippingFeesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shipping_fees', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('province_city', 200)->nullable();
+            $table->float('shipping_fee')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('shipping_fees');
+    }
+}
